6913892e7473f546c236067a62a2e9ea8c75c60b
Loco Mia <stikmenamerica@gmail.com>
2025-07-23 00:41:10 +0300

    + print_bool function

--- printer.h
+++ printer.h
@@ -5,4 +5,5 @@ void rclf_print_all(RclfDocument *doc);
 void rclf_print_column(RclfDocument *doc, int col_index);
 void rclf_print_key(RclfKey key, int col_index, int key_index, int quote_pos);
 void rclf_print_value(RclfValue val, int col_index, int key_index, int value_index, int quote_pos);
+void print_bool(RclfValue *val, int col, int key, int val_dx, int indent);
 #endif
